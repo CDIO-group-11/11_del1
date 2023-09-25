b5d841199965dea844641bc9b2bcbe4c84ad4942
dendersen <dendersen@endersen.dk>
2023-09-25 15:36:09 +0200

    simple dice test

--- src/App/test/Test.java
+++ src/App/test/Test.java
@@ -1,7 +1,27 @@
 package App.test;
 
+import App.spil.RaffleCup;
+
 public class Test {
+  private static RaffleCup cup = new RaffleCup(2, 6); 
   public static void main(String[] args) {
-    System.out.println("Hello, World!");
+    int[] sides = new int[6];
+    for (int i = 0; i < 1000; i++) {
+      cup.roll();
+      for (int j : cup.getSides()) {
+        sides[j-1]++;
+      }
+    }
+    double mean = 0;
+    for (int i = 0; i < sides.length; i++) {
+      mean += sides[i];
+    }
+    mean /= (double)sides.length;
+    double deviation = 0;
+    for (int i = 0; i < sides.length; i++) {
+      deviation += Math.pow(sides[i]-mean,2);
+    }
+    deviation = Math.sqrt(deviation*1d/(double)sides.length);
+    System.out.println(deviation);
   }
 }
