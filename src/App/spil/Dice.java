218fcbe6a8b0bf41a8ce9029eec5a76a3857b6f8
dendersen <dendersen@endersen.dk>
2023-09-25 15:09:35 +0200

    dice can now roll

--- src/App/spil/Dice.java
+++ src/App/spil/Dice.java
@@ -2,7 +2,11 @@ package App.spil;
 
 public class Dice {
   int sides;
+  int activeSide = 1;
   public Dice(int sides){
     this.sides = sides;
+  }
+  public void roll(){
+    activeSide = (int)(Math.random() * (sides)) + 1;
   }
 }
