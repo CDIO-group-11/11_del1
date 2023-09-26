0e289ad88430a9ad5560d5950b8415660a28fcf2
dendersen <dendersen@endersen.dk>
2023-09-26 16:02:51 +0200

    added more comments

--- src/App/spil/Main.java
+++ src/App/spil/Main.java
@@ -11,15 +11,22 @@ public class Main {
   private static boolean currentPlayer = true;//true == 1
   private static boolean p1,p2;
   public static void main(String[] args) {
+    //clears console
     System.out.print("\033[H\033[2J");
     System.out.flush();
+    //tells user how to play
     System.out.println("to Roll the Dice type:\"" + ROLL_COMMAND + "\"");
+    //removes conflict from moving up
     System.out.println("\n\n\n\n\n\n\n\n");
     while(true){
+      //prints curent player and moves the console cursor  up to the top
       System.out.println("\r\033[9Acurrent player: " + g() +(currentPlayer ? "1" : "2") + reset());
       System.out.print("        \r");
+      
+      //waits and then rolls
       awaitRoll();
       cup.roll();
+      
       //adds dice to points
       if(currentPlayer){
         player1 += cup.getSides()[0];
@@ -62,37 +69,54 @@ public class Main {
     }
   }
   private static void awaitRoll(){
-    while (true){
+    while (true){//escapes if player gives correct input
       String in = scan.nextLine();
       if(in.equals(ROLL_COMMAND)){
         return;
       }else{
+        //removes the input the user wrote
         System.out.print("\033[1A" + " ".repeat(in.length()) + "\r");
       }
     }
   }
   private static void prettyPrint(){
+    //prints  player 1 points
     System.out.println(reset() + "Player " + g() + 1 + reset() + " (" + b() + player1 + "/40 " + p() + "point" + reset() + ")");
+    //prints player 2 points
     System.out.println(reset() + "Player " + g() + 2 + reset() + " (" + b() + player2 + "/40 " + p() + "point" + reset() + ")\n");
+    
     System.out.println("Roll:");
+    //prints the number on dice 1
     System.out.println("Dice " + b() + "1" + p() +":  " + g() + cup.getSides()[0] + reset());
+    //prints the number on dice 2
     System.out.println("Dice " + b() + "2" + p() +":  " + g() + cup.getSides()[1] + reset());
+    //finds the sum of sides
     int sum = cup.getSides()[0] + cup.getSides()[1];
+    //prints sum of sides
     System.out.println("Sum of Dice: " + g() + sum + reset() + " ");
   }
+  /**
+   * @return the ansi value that changes console text color to green
+   */
   private static String g(){
     return "\u001b[32m";
   }
+    /**
+   * @return the ansi value that resets the color of text in the console
+   */
   private static String reset(){
     return "\u001b[0m";
   }
+    /**
+   * @return the ansi value that changes console text color to blue
+   */
   private static String b(){
     return "\u001b[34m";
   }
+    /**
+   * @return the ansi value that changes console text color to purple
+   */
   private static String p(){
     return "\u001b[35m";
-  }
-  public static RaffleCup getCup() {
-    return cup;
   }
 }
