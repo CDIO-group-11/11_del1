f6e506a67d00ac496805a76ec310ecdbc2d3e82f
dendersen <dendersen@endersen.dk>
2023-09-28 12:51:14 +0200

    remade test

--- src/App/test/Test.java
+++ src/App/test/Test.java
@@ -1,15 +1,18 @@
 package App.test;
 
+import App.spil.Main;
 import App.spil.RaffleCup;
 
 public class Test {
   private static RaffleCup cup = new RaffleCup(2, 6); 
   public static void main(String[] args) {
-    isFair();
+    int runCount = 1;
+    isFast(runCount);
+    isFair(runCount);
   }
-  private static void isFair() {
+  private static void isFair(int runCount) {
     int[] sides = new int[6];
-    for (int i = 0; i < 1000; i++) {
+    for (int i = 0; i < runCount; i++) {
       cup.roll();
       for (int j : cup.getSides()) {
         sides[j-1]++;
@@ -25,18 +28,37 @@ public class Test {
       deviation += Math.pow(sides[i]-mean,2);
     }
     deviation = Math.sqrt(deviation*1d/(double)sides.length);
-    if(deviation < 20 && mean < 340 && mean > 320){
-      pass("dice fairness");
+    if(deviation < (runCount/50f) && mean < ((runCount/3f)*1.1f) && mean > ((runCount/3f)*0.9f)){
+      pass("dice fairness\n\tmean: " + mean +"\n\tdeviation: " + deviation);
     }else{
-      fail("dice fairnes");
+      fail("dice fairnes\n\tmean: " + mean +"\n\tdeviation: " + deviation);
+    }
+  }
+  private static void isFast(int runCount) {
+    RaffleCup cup = new RaffleCup(2, 6);
+    long start = System.currentTimeMillis();
+    for (int i = 0; i < runCount; i++) {
+      cup.roll();
+      cup.getSides();
+      Main.prettyPrint();
+      System.out.print("\r\033[10A");
+    }
+    System.out.println("\n".repeat(3));
+    System.out.print("\033[H\033[2J");
+    System.out.flush();
+    long end = System.currentTimeMillis();
+    if(end-start < 333.333f*runCount){
+      pass("dice speed\n\ttime taken: " + (end-start) + "ms\n\tallowed: " + (333.333f*runCount) + "ms");
+    }else{
+      fail("dice speed\n\ttime taken: " + (end-start) + "ms\n\tallowed: " + (333.333f*runCount) + "ms");
     }
   }
   private static void pass(String test){
     System.out.println(test);
-    System.out.println(" \u001b[32mPASSED\u001b[0m");
+    System.out.println("\t\t\u001b[32mPASSED\u001b[0m");
   }
   private static void fail(String test){
     System.out.println(test);
-    System.out.println(" \u001b[31mFAILLED\u001b[0m");
+    System.out.println("\t\t\u001b[31mFAILLED\u001b[0m");
   }
 }
