8a9fc8170a750822ee02baf61ac4013865850949
dendersen <dendersen@endersen.dk>
2023-09-29 12:42:08 +0200

    small bugfix

--- run.bat
+++ run.bat
@@ -1,7 +1,7 @@
-echo 0m[35m"if this text is not putple please use runClean.bat instead"[0m
+echo 0m[35m"if there are problems with wierd characters in the program use runClean.bat instead"[0m
 javac -sourcepath ./src/ -d ./bin/ ./src/App/spil/Main.java
-echo 0m[35m"if this text is not putple please use runClean.bat instead"[0m
+echo 0m[35m"if there are problems with wierd characters in the program use runClean.bat instead"[0m
 cd bin
-echo 0m[35m"if this text is not putple please use runClean.bat instead"[0m
+echo 0m[35m"if there are problems with wierd characters in the program use runClean.bat instead"[0m
 java App/spil/Main
 Pause
--- runClean.bat
+++ runClean.bat
@@ -1,7 +1,4 @@
-echo 0m[35m"if this text is putple please use run.bat instead"[0m
 javac -sourcepath ./src/ -d ./bin/ ./src/App/spil/Main.java
-echo 0m[35m"if this text is putple please use run.bat instead"[0m
 cd bin
-echo 0m[35m"if this text is putple please use run.bat instead"[0m
 java App/spil/Main noCol
 Pause
